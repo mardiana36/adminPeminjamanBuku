875f5bec8318daca759a1ca6f2b26e22ea1a0302
mardiana36 <kelikimardiana@gmail.com>
2024-12-29 22:56:42 +0800

    navbar

--- uas/Properties/Resources.Designer.cs
+++ uas/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace uas.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap userMenu {
+            get {
+                object obj = ResourceManager.GetObject("userMenu", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
