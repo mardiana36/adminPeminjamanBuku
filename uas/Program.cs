313e5d579ce0104495b4f5066dc3141c2f920766
mardiana36 <kelikimardiana@gmail.com>
2025-01-03 13:45:14 +0800

    perbaikan

--- uas/Program.cs
+++ uas/Program.cs
@@ -16,7 +16,7 @@ namespace uas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Anggota());
+            Application.Run(new Form1());
         }
     }
 }
